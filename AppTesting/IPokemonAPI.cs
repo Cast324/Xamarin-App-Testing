63a35825999721e1e4b479d65b5b8c0e4b0cfeee
Michael Blades <michaelblades12@gmail.com>
2021-09-05 21:52:50 -0500

    Added logic for pulling in pokemon from api
    Can specify pokemon name in entry

--- AppTesting/IPokemonAPI.cs
+++ AppTesting/IPokemonAPI.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
 namespace AppTesting
 {
     public interface IPokemonAPI
-    { 
-
+    {
+        [Get("/{pokemon}")]
+        Task<PokemonModel> GetPokemon(string pokemon);
     }
 }
--- AppTesting/MainPage.xaml.cs
+++ AppTesting/MainPage.xaml.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Polly;
+using Refit;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -10,13 +15,34 @@ namespace AppTesting
 {
     public partial class MainPage : ContentPage
     {
+        public PokemonModel pokemon { get; set; }
         public MainPage()
         {
             InitializeComponent();
         }
 
-        void Button_Clicked(System.Object sender, System.EventArgs e)
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
         {
+            await GetPokemon();
+            pokemonImage.Source = ImageSource.FromUri(new Uri("https://cdn.cnn.com/cnnnext/dam/assets/210226041421-02-pokemon-anniversary-design.jpg"));
+        }
+
+        async Task GetPokemon()
+        {
+            var fallbackPolicy = Policy<PokemonModel>.Handle<ApiException>(ex => ex.StatusCode == HttpStatusCode.NotFound).FallbackAsync(HandleException);
+            pokemon = await fallbackPolicy.ExecuteAsync(GetPokemonAsync);
+        }
+
+        private async Task<PokemonModel> GetPokemonAsync()
+        {
+            var apiResponse = RestService.For<IPokemonAPI>("https://pokeapi.co/api/v2/pokemon");
+            return await apiResponse.GetPokemon(pokeNameEntry.Text);
+        }
+
+        private Task<PokemonModel> HandleException(CancellationToken arg)
+        {
+            Console.WriteLine("404 was returned check name of pokemon!");
+            return Task.FromResult<PokemonModel>(default);
         }
     }
 }
